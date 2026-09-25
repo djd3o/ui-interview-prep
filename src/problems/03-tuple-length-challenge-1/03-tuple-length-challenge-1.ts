2945191b55698adaaecb509d75ed242c2f784e2e
Amadeo <its.amad3o@gmail.com>
2026-09-25 14:11:43 -0700

    Completed throttle

--- src/problems/03-tuple-length-challenge-1/03-tuple-length-challenge-1.ts
+++ src/problems/03-tuple-length-challenge-1/03-tuple-length-challenge-1.ts
@@ -16,12 +16,15 @@ import type { Equal, Expect } from '@course/types'
 
 /* _____________ Your Code Here _____________ */
 
-type Length = {};
+type Length<T extends readonly any[]> = T['length']
 
 /* _____________ Test Cases _____________ */
 
+Array['length']
 const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
 const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+
+type teslaLength = Length<typeof tesla>
 
 type cases = [
   Expect<Equal<Length<typeof tesla>, 4>>,
